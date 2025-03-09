fd644072492860ff1f446999dc2e313b80d07a1d
Angelo Arcillas <angeloarcillas64@gmail.com>
2025-03-09 20:32:16 +0800

    Implement link fetch requests

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,38 +1,66 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 type Link = {
   id: number;
-  url: string;
+  short_code: string;
+  original_url: string;
+  expires_at: string;
   total_clicks: number;
-}
-
-const CreateShortenLink = () => {
-  return (
-    <section className="space-y-6">
-      <h1 className="text-4xl font-extrabold">Paste the URL to be shortened</h1>
-      <div className="flex max-sm:flex-col items-center gap-2">
-        <input type="text" className="input" />
-        <button className="button w-1/4 max-sm:w-full ">Shorten URL</button>
-      </div>
-    </section>
-  );
 }
 
 const sortedLinks = (links: Link[]) => {
   return links.sort((a, b) => b.total_clicks - a.total_clicks)
 }
 
-const ShortenLinks = ({ links }: { links: Link[] }) => {
+const CreateShortenLink = ({ generateShortenLink }: { generateShortenLink: (event: any) => void }) => {
+  return (
+    <section className="space-y-6">
+      <h1 className="text-4xl font-extrabold">Paste the URL to be shortened</h1>
+      <form onSubmit={generateShortenLink} className="flex max-sm:flex-col items-center gap-2">
+        <input type="text" name="url" className="input" />
+        <button className="button w-1/4 max-sm:w-full ">Shorten URL</button>
+      </form>
+    </section>
+  );
+}
+
+const ShortenLink = ({ link, updateTotalClicks }: { link: Link, updateTotalClicks: (link: Link) => void }) => {
+  const parsedDate = (createdAt: string) => {
+    const date = new Date(createdAt);
+
+    return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`
+  }
+
+  return (
+    <button onClick={() => updateTotalClicks(link)} className="cursor-pointer bg-teal-200 p-2 rounded-md  hover:bg-teal-400 w-full space-y-2">
+      <p className="text-left">
+        https://zerexei.dev/{link.short_code} ({link.original_url})
+      </p>
+
+      <div className="flex items-center justify-between">
+        <p>expires at: {parsedDate(link.expires_at)}</p>
+        <p className="border rounded px-2 text-xs font-semibold border-teal-600 text-teal-700">{link.total_clicks}</p>
+      </div>
+    </button>
+  )
+
+}
+
+const ShortenLinks = ({ links, updatePopularLinkTotalClicks }: {
+  links: Link[],
+  updatePopularLinkTotalClicks: (link: Link) => void
+}) => {
   return (
     <section>
       <h2 className="text-2xl font-semibold mb-4">Popular Links</h2>
       <ul className="space-y-2 max-h-[25rem] overflow-y-auto pr-2">
         {sortedLinks(links).map(link => (
-          <li className="">
-            <a href={link.url} className="cursor-pointer bg-teal-200 p-2 rounded-md flex items-center justify-between hover:bg-teal-400">
-              <p>{link.url}</p>
-              <p className="border rounded px-2 text-xs font-semibold border-teal-600 text-teal-700">{link.total_clicks}</p>
-            </a>
+          <li className="" key={link.id}>
+            <ShortenLink
+              link={link}
+              updateTotalClicks={updatePopularLinkTotalClicks}
+            />
           </li>
         ))}
       </ul>
@@ -40,18 +68,20 @@ const ShortenLinks = ({ links }: { links: Link[] }) => {
   );
 }
 
-const GeneratedShortenLink = ({ link }: { link: Link }) => {
+const GeneratedShortenLink = ({ link, updateGeneratedLinkTotalClicks }: { link: Link, updateGeneratedLinkTotalClicks: (link: Link) => void }) => {
+  const copy = () => {
+    navigator.clipboard.writeText(`https://zerexei.dev/${link.short_code}`)
+  }
   return (
     <section>
       <h2 className="text-2xl font-semibold mb-4">Generated Link</h2>
 
       <div className="flex items-center gap-2">
-        <a href={link.url} className="cursor-pointer bg-teal-200 p-2 rounded-md flex items-center justify-between hover:bg-teal-400 w-full">
-          <p>{link.url}</p>
-          <p className="border rounded px-2 text-xs font-semibold border-teal-600 text-teal-700">{link.total_clicks}</p>
-        </a>
-        
-        <button className="button">Copy</button>
+        <ShortenLink
+          link={link}
+          updateTotalClicks={updateGeneratedLinkTotalClicks}
+        />
+        <button onClick={copy} className="button">Copy</button>
       </div>
     </section>
   )
@@ -59,36 +89,75 @@ const GeneratedShortenLink = ({ link }: { link: Link }) => {
 
 
 const App = () => {
-  const [generatedShortenLink, setGeneratedShortenLink] = useState<Link>({id: 1, url: 'asdasd', total_clicks: 123} as Link);
+  const [generatedShortenLink, setGeneratedShortenLink] = useState<Link>({} as Link);
+  const [shortenLinks, setShortenLinks] = useState<Link[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
 
-  const [shortenLinks, setShortenLinks] = useState<Link[]>([
-    { id: 1, url: "https://short-link.com/abc1231", total_clicks: 1 },
-    { id: 2, url: "https://short-link.com/abc1232", total_clicks: 12 },
-    { id: 3, url: "https://short-link.com/abc1233", total_clicks: 123 },
-    { id: 433, url: "https://short-link.com/abc1234", total_clicks: 1234 },
-    { id: 5232, url: "https://short-link.com/abc1235", total_clicks: 12345 },
-    { id: 522, url: "https://short-link.com/abc1235", total_clicks: 12345 },
-    { id: 115, url: "https://short-link.com/abc1235", total_clicks: 12345 },
-    { id: 65, url: "https://short-link.com/abc1235", total_clicks: 12345 },
-    { id: 55, url: "https://short-link.com/abc1235", total_clicks: 12345 },
-    { id: 45, url: "https://short-link.com/abc1235", total_clicks: 12345 },
-    { id: 35, url: "https://short-link.com/abc1235", total_clicks: 12345 },
-    { id: 25, url: "https://short-link.com/abc1235", total_clicks: 12345 },
-    { id: 15, url: "https://short-link.com/abc1235", total_clicks: 12345 },
-    { id: 58, url: "https://short-link.com/abc1235", total_clicks: 12345 },
-    { id: 57, url: "https://short-link.com/abc1235", total_clicks: 12345 },
-    { id: 56, url: "https://short-link.com/abc1235", total_clicks: 12345 },
-    { id: 55, url: "https://short-link.com/abc1235", total_clicks: 12345 },
-    { id: 54, url: "https://short-link.com/abc1235", total_clicks: 12345 },
-    { id: 53, url: "https://short-link.com/abc1235", total_clicks: 12345 },
-    { id: 52, url: "https://short-link.com/abc1235", total_clicks: 12345 },
-  ]);
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      const res = await axios('http://localhost:5000/links');
+      const links: Link[] = res.data
+      setShortenLinks(links);
+      setLoading(false);
+    })()
+  }, [])
+
+  const visit = async (link: Link) => {
+    const res = await axios.patch("http://localhost:5000/links", link);
+    return res.data
+  }
+
+  const updateGeneratedLinkTotalClicks = async (link: Link) => {
+    setLoading(true);
+    link = await visit(link)
+    setGeneratedShortenLink(link);
+    setLoading(false);
+  }
+  const updatePopularLinkTotalClicks = async (target: Link) => {
+    setLoading(true);
+    target = await visit(target)
+
+    setShortenLinks((prevLinks) =>
+      prevLinks.map((link) =>
+        link.id === target.id ? { ...link, ...target } : link
+      )
+    );
+    setLoading(false);
+  }
+
+  const generateShortenLink = async (event: any) => {
+    setLoading(true);
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const url = formData.get("url");
+
+    const res = await axios.post('http://localhost:5000/links', { url })
+    setGeneratedShortenLink(res.data)
+    setLoading(false);
+  }
   return (
     <>
       <main className="space-y-6">
-        <CreateShortenLink />
-        {generatedShortenLink.id ? <GeneratedShortenLink link={generatedShortenLink} /> : ""}
-        {shortenLinks.length ? <ShortenLinks links={shortenLinks} /> : ""}
+        {
+          loading
+            ? <div className="fixed inset-0 bg-black/10 text-4xl font-extrabold flex items-center justify-center">Loading....</div>
+            : ""
+        }
+
+        <CreateShortenLink generateShortenLink={generateShortenLink} />
+
+        {
+          generatedShortenLink.id
+            ? <GeneratedShortenLink link={generatedShortenLink} updateGeneratedLinkTotalClicks={updateGeneratedLinkTotalClicks} />
+            : ""
+        }
+
+        {
+          shortenLinks.length
+            ? <ShortenLinks links={shortenLinks} updatePopularLinkTotalClicks={updatePopularLinkTotalClicks} />
+            : ""
+        }
       </main>
     </>
   )
